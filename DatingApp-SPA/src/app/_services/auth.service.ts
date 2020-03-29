b55a6cc309d271e87797d480f90530c7c41453ab
Fabio Nan <fabio.nan@finsa.it>
2020-03-29 10:00:06 +0200

    [SPA]Add login/logout methods

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map} from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+
+constructor(private http: HttpClient) {}
+
+  user: any = {};
+
+login(model: any){
+  return this.http.post(this.baseUrl + 'login', model)
+    .pipe(
+      map((response :any) => {
+        const user = response;
+        if(user){
+          localStorage.setItem('token',user.token);
+        }
+      })
+    );
+  }
+
+loggedIn(){
+  const token = localStorage.getItem('token');
+  return !!token;
+}
+
+logout(){
+  localStorage.removeItem('token');
+  console.log('logged out');
+}
+
+}
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { error } from 'util';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +11,17 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login(){
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(
+      next => {console.log('logged ok');
+    }, error => {
+      console.log('failed to login');
+    });
   }
 
 }
