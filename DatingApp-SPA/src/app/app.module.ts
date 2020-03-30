b136ddab3a25a15f3eeb61febeb1a1c9e4fc5398
Fabio Nan <fabio.nan@finsa.it>
2020-03-30 12:50:29 +0200

    [SPA] Adding login/register conditional

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      NavComponent
+      NavComponent,
+      RegisterComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any ={}; 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register(){
+    console.log(this.model);
+  }
+
+  cancel(){
+    console.log("cancelled");
+  }
+  //8:11 MIN
+}
